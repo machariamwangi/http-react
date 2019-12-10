d15885eb1bf1882be8996ec3c6abb425f917bdfe
root <elijahmacharia54@gmail.com>
2019-12-10 15:47:42 -0500

    src/ Multiple Layout In React Using React Router V4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
-import DisplayList from './components/DisplayList';
-import UserForm from './components/UserForm';
+// import DisplayList from './components/DisplayList';
+// import UserForm from './components/UserForm';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';  
+  
+/** Layouts **/  
+import LoginLayoutRoute from "./components/LoginLayout";  
+import DashboardLayoutRoute from "./components/DashboardLayout";  
+  
+/** Components **/  
+import UserPage from './components/UserPage';  
+import LoginPage from './components/LoginPage';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-    {/* <DisplayList /> */}
-    <UserForm />
-    </div>
-  );
-}
-
-export default App;
+class App extends Component {  
+  render() {  
+    return (  
+      <Router>  
+        <Switch>  
+          <Route exact path="/">  
+            <Redirect to="/layout1" />  
+          </Route>  
+          <LoginLayoutRoute path="/layout1" component={LoginPage} />  
+          <DashboardLayoutRoute  path="/layout2" component={UserPage} />  
+        </Switch>  
+      </Router>  
+    );  
+  }  
+}  
+  
+export default App;   
